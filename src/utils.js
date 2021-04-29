4aa08a782a7dc82d90a874db0235a3a826fe683b
jashgopani <jash.gopani@somaiya.edu>
2021-04-29 21:56:15 +0530

    Organised code in utils file

--- src/utils.js
+++ src/utils.js
@@ -1,45 +1,16 @@
 import Color from "jscolor";
 
-export function getContrastingColor(color) {
-    let d = 0;
-
-    // Counting the perceptive luminance - human eye favors green color...
-    const luminance = (0.299 * color.r + 0.587 * color.g + 0.114 * color.b) / 255;
-
-    if (luminance > 0.5) d = 31;
-    // bright colors - black font
-    else d = 255; // dark colors - white font
-
-    d = rgbToHex(d, d, d); //convert d to hex
-    return new Color(d);
-}
-
-export function random() {
-    return Math.floor(Math.random() * 100) % 255;
-}
-
+/*Get random Integer from a range*/
 export function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-export function rgbToHex(r, g, b) {
-    r = r.toString(16);
-    g = g.toString(16);
-    b = b.toString(16);
-    if (r.length == 1) r = "0" + r;
-    if (g.length == 1) g = "0" + g;
-    if (b.length == 1) b = "0" + b;
-    return "#" + r + g + b;
-}
-export function randomHex() {
-    let rand = new Color("#ffffff");
-    rand.h = getRandomInt(0, 360);
-    rand.s = getRandomInt(20, 100);
-    rand.l = getRandomInt(20, 100);
-    return rand.hex;
-}
 
+/**
+ * Generate a randomColor object
+ * @returns Color
+ */
 export function randomColor() {
     let color = new Color();
     color.h = getRandomInt(0, 360);
@@ -48,7 +19,12 @@ export function randomColor() {
     return color;
 }
 
-
+/**
+ * Generate Theme Color from a given color
+ * @param {Color} color Primary Color object of theme
+ * @param {Boolean} oppositeAccent Should the accent color be contrasting
+ * @returns Theme Object with primary,secondary and accent color objects
+ */
 export function generateThemeColors(color, oppositeAccent) {
     const primary = new Color(color.hex);
     const secondary = new Color(color.hex);
@@ -71,10 +47,135 @@ export function generateThemeColors(color, oppositeAccent) {
     };
 }
 
+/**
+ * Generate Random theme using random colors utility methods
+ * @returns Random Theme
+ */
 export function randomTheme() {
     return generateThemeColors(randomColor(), Math.random([true, false]));
 }
 
+/**
+ * Get basic style for element based on the passed background color
+ * @param {String} elementBackground color to be set as the background color of application[primary/secondary/accent]
+ * @param {Object} currentTheme current theme of the application
+ * @param {String} specificFontColor color to be used as font color for the element [primary/secondary/accent]
+ * @returns Object containing CSS properties
+ */
+export function getBasicStyle(
+    elementBackground,
+    currentTheme,
+    specificFontColor
+) {
+    //set current bg
+    const backgroundColor = currentTheme[elementBackground].hex;
+
+    //default font color will be contrasting color of background color
+    //if we explicitly pass a color for font, we set that
+    const oppositeStyle = getOppositeStyle(elementBackground);
+    let fontColor = getContrastingColor(elementBackground).hex;
+    if (fontColor !== null) {
+        fontColor = currentTheme[specificFontColor].hex;
+    }
+
+    return {
+        backgroundColor,
+        color: fontColor,
+    };
+}
+
+/**
+ * Convert RGB values to Hex String
+ * @param {Number} r red(0-255)
+ * @param {Number} g green(0-255)
+ * @param {Number} b blue(0-255)
+ * @returns String
+ */
+export function rgbToHex(r, g, b) {
+    r = r.toString(16);
+    g = g.toString(16);
+    b = b.toString(16);
+    if (r.length == 1) r = "0" + r;
+    if (g.length == 1) g = "0" + g;
+    if (b.length == 1) b = "0" + b;
+    return "#" + r + g + b;
+}
+
+/**
+ * Get opposite value of number in a given range
+ * @param {Number} num Number whose opposite value is to be calculated
+ * @param {Number} min Starting value of range
+ * @param {Number} max Ending Value of range
+ * @returns Number
+ */
+function oppositeValueInRange(num, min, max) {
+    return min + (max - num);
+}
+
+
+/**
+ * Get brightness value of color
+ * @param {Color} color Color whose luminance is to be calculated
+ * @returns Float
+ */
+function luminance(color) {
+    const r = color.r;
+    const g = color.g;
+    const b = color.b;
+
+    var a = [r, g, b].map(function (v) {
+        v /= 255;
+        return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
+    });
+    return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+}
+
+/**
+ * Calculate the contrast between 2 colors
+ * @param {Color} color1 First Color Object
+ * @param {Color} color2 Second Color Object
+ * @returns Float
+ */
+export function contrast(color1, color2) {
+    var lum1 = luminance(color1);
+    var lum2 = luminance(color2);
+    var brightest = Math.max(lum1, lum2);
+    var darkest = Math.min(lum1, lum2);
+    return (brightest + 0.05) / (darkest + 0.05);
+}
+
+/**
+ * Get contrasting color for a given color
+ * @param {Color} color Color object
+ * @returns Color
+ */
+export function getContrastingColor(color) {
+    let d = 0;
+
+    // Counting the perceptive luminance - human eye favors green color...
+    const luminance = (0.299 * color.r + 0.587 * color.g + 0.114 * color.b) / 255;
+
+    if (luminance > 0.5) d = 31;
+    // bright colors - black font
+    else d = 255; // dark colors - white font
+
+    d = rgbToHex(d, d, d); //convert d to hex
+    return new Color(d);
+}
+
+/**
+ * Given a particular component of theme, get the contrasting one
+ * @param {String} style String specifying existing style (primary/secondary/accent)
+ * @returns String
+ */
+export function getOppositeStyle(style) {
+    if (style === "primary") return "secondary";
+    else if (style === "secondary") return "primary";
+    return "secondary";
+}
+
+
+//might be discarded
 export function getCss(style, theme, outline) {
     let backgroundColor, color;
     const oppositeColor =
@@ -103,53 +204,4 @@ export function getCss(style, theme, outline) {
         color,
         border: outline ? "2px solid " + color : "none",
     };
-}
-
-/* Get basic style for element based on the passed background color */
-export function getBasicStyle(elementBackground, currentTheme, specificFontColor) {
-    //set current bg
-    const backgroundColor = currentTheme[elementBackground].hex;
-
-    //default font color will be contrasting color of background color
-    //if we explicitly pass a color for font, we set that
-    const oppositeStyle = getOppositeStyle(elementBackground);
-    let fontColor = getContrastingColor(elementBackground).hex;
-    if (fontColor !== null) {
-        fontColor = currentTheme[specificFontColor].hex;
-    }
-
-    return {
-        backgroundColor,
-        color: fontColor
-    }
-}
-
-function oppositeValueInRange(num, min, max) {
-    return min + (max - num);
-}
-
-function luminance(color) {
-    const r = color.r;
-    const g = color.g;
-    const b = color.b;
-
-    var a = [r, g, b].map(function (v) {
-        v /= 255;
-        return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
-    });
-    return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
-}
-
-export function contrast(color1, color2) {
-    var lum1 = luminance(color1);
-    var lum2 = luminance(color2);
-    var brightest = Math.max(lum1, lum2);
-    var darkest = Math.min(lum1, lum2);
-    return (brightest + 0.05) / (darkest + 0.05);
-}
-
-export function getOppositeStyle(style) {
-    if (style === "primary") return "secondary";
-    else if (style === "secondary") return "primary";
-    return "secondary";
 }
