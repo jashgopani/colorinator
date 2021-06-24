3f6e88432a0c2ec393152f05d7ac5734ff38905f
jashgopani <jash.gopani@somaiya.edu>
2021-06-24 12:05:52 +0530

    more flat primary colors will be generated now and minor sonar fixes

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
-import Color from "jscolor";
+import Color from 'jscolor';
 
 /*Get random Integer from a range*/
 export function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
@@ -12,11 +12,11 @@ export function getRandomInt(min, max) {
  * @returns Color
  */
 export function randomColor() {
-    let color = new Color();
-    color.h = getRandomInt(0, 360);
-    color.s = getRandomInt(20, 100);
-    color.l = getRandomInt(20, 100);
-    return color;
+	let color = new Color();
+	color.h = getRandomInt(0, 360);
+	color.s = getRandomInt(80, 90);
+	color.l = getRandomInt(60, 70);
+	return color;
 }
 
 /**
@@ -26,25 +26,25 @@ export function randomColor() {
  * @returns Theme Object with primary,secondary and accent color objects
  */
 export function generateThemeColors(color, oppositeAccent) {
-    const primary = new Color(color.hex);
-    const secondary = new Color(color.hex);
-    const accent = new Color(color.hex);
+	const primary = new Color(color.hex);
+	const secondary = new Color(color.hex);
+	const accent = new Color(color.hex);
 
-    //for secondary color
-    secondary.s = getRandomInt(5, 10);
-    secondary.l = getRandomInt(95, 100);
+	//for secondary color
+	secondary.s = getRandomInt(5, 10);
+	secondary.l = getRandomInt(95, 100);
 
-    //for accent color
-    const sign = Math.random([1, -1]);
-    if (!oppositeAccent) accent.h = primary.h + sign * getRandomInt(30, 40);
-    else accent.h = oppositeValueInRange(primary.h, 0, 255);
-    accent.l = primary.l + getRandomInt(5, 10);
+	//for accent color
+	const sign = Math.random([1, -1]);
+	if (!oppositeAccent) accent.h = primary.h + sign * getRandomInt(30, 40);
+	else accent.h = oppositeValueInRange(primary.h, 0, 255);
+	accent.l = primary.l + getRandomInt(5, 10);
 
-    return {
-        primary,
-        secondary,
-        accent,
-    };
+	return {
+		primary,
+		secondary,
+		accent,
+	};
 }
 
 /**
@@ -52,7 +52,7 @@ export function generateThemeColors(color, oppositeAccent) {
  * @returns Random Theme
  */
 export function randomTheme() {
-    return generateThemeColors(randomColor(), Math.random([true, false]));
+	return generateThemeColors(randomColor(), Math.random([true, false]));
 }
 
 /**
@@ -63,25 +63,25 @@ export function randomTheme() {
  * @returns Object containing CSS properties
  */
 export function getBasicStyle(
-    elementBackground,
-    currentTheme,
-    specificFontColor
+	elementBackground,
+	currentTheme,
+	specificFontColor
 ) {
-    //set current bg
-    const backgroundColor = currentTheme[elementBackground].hex;
+	//set current bg
+	const backgroundColor = currentTheme[elementBackground].hex;
 
-    //default font color will be contrasting color of background color
-    //if we explicitly pass a color for font, we set that
-    const oppositeStyle = getOppositeStyle(elementBackground);
-    let fontColor = getContrastingColor(currentTheme[elementBackground]).hex;
-    if (specificFontColor !== null) {
-        fontColor = currentTheme[specificFontColor].hex;
-    }
+	//default font color will be contrasting color of background color
+	//if we explicitly pass a color for font, we set that
+	const oppositeStyle = getOppositeStyle(elementBackground);
+	let fontColor = getContrastingColor(currentTheme[elementBackground]).hex;
+	if (specificFontColor !== null) {
+		fontColor = currentTheme[specificFontColor].hex;
+	}
 
-    return {
-        backgroundColor,
-        color: fontColor,
-    };
+	return {
+		backgroundColor,
+		color: fontColor,
+	};
 }
 
 /**
@@ -92,15 +92,14 @@ export function getBasicStyle(
  * @returns String
  */
 export function rgbToHex(r, g, b) {
-    r = r.toString(16);
-    g = g.toString(16);
-    b = b.toString(16);
-    if (r.length == 1) r = "0" + r;
-    if (g.length == 1) g = "0" + g;
-    if (b.length == 1) b = "0" + b;
-    return "#" + r + g + b;
+	r = r.toString(16);
+	g = g.toString(16);
+	b = b.toString(16);
+	if (r.length === 1) r = '0' + r;
+	if (g.length === 1) g = '0' + g;
+	if (b.length === 1) b = '0' + b;
+	return '#' + r + g + b;
 }
-
 
 /**
  * Get contrasting color for a given color
@@ -108,17 +107,18 @@ export function rgbToHex(r, g, b) {
  * @returns Color
  */
 export function getContrastingColor(color) {
-    let d = 0;
+	let d = 0;
 
-    // Counting the perceptive luminance - human eye favors green color...
-    const luminance = (0.299 * color.r + 0.587 * color.g + 0.114 * color.b) / 255;
+	// Counting the perceptive luminanceValue - human eye favors green color...
+	const luminanceValue =
+		(0.299 * color.r + 0.587 * color.g + 0.114 * color.b) / 255;
 
-    if (luminance > 0.5) d = 31;
-    // bright colors - black font
-    else d = 255; // dark colors - white font
+	if (luminanceValue > 0.5) d = 31;
+	// bright colors - black font
+	else d = 255; // dark colors - white font
 
-    d = rgbToHex(d, d, d); //convert d to hex
-    return new Color(d);
+	d = rgbToHex(d, d, d); //convert d to hex
+	return new Color(d);
 }
 
 /**
@@ -127,41 +127,45 @@ export function getContrastingColor(color) {
  * @returns String
  */
 export function getOppositeStyle(style) {
-    if (style === "primary") return "secondary";
-    else if (style === "secondary") return "primary";
-    return "secondary";
+	if (style === 'primary') return 'secondary';
+	else if (style === 'secondary') return 'primary';
+	return 'secondary';
 }
-
 
 //might be discarded
 export function getCss(style, theme, outline) {
-    let backgroundColor, color;
-    const oppositeColor =
-        style === "primary"
-            ? "secondary"
-            : style === "accent"
-                ? "accent"
-                : "primary";
-    if (style) {
-        backgroundColor = outline ? "transparent" : theme[style].hex;
-        color = outline
-            ? theme[style].hex
-            : getContrastingColor(
-                style === oppositeColor ? theme[oppositeColor] : theme[style]
-            ).hex;
-    }
+	let backgroundColor,
+		color = theme[style].hex;
+	let oppositeColor;
+	if (style === 'primary') {
+		oppositeColor = 'secondary';
+	} else {
+		oppositeColor = style === 'accent' ? 'accent' : 'primary';
+	}
 
-    if (style === "accent" && outline) {
-        console.log("accent button util", outline);
-        backgroundColor = getContrastingColor(theme.accent);
-        color = theme.accent.hex;
-    }
+	if (style) {
+		backgroundColor = outline ? 'transparent' : theme[style].hex;
 
-    return {
-        backgroundColor,
-        color,
-        border: outline ? "2px solid " + color : "none",
-    };
+		if (outline) {
+			if (style === oppositeColor) {
+				color = getContrastingColor(theme[oppositeColor]).hex;
+			} else {
+				color = getContrastingColor(theme[style]).hex;
+			}
+		}
+	}
+
+	if (style === 'accent' && outline) {
+		console.log('accent button util', outline);
+		backgroundColor = getContrastingColor(theme.accent);
+		color = theme.accent.hex;
+	}
+
+	return {
+		backgroundColor,
+		color,
+		border: outline ? '2px solid ' + color : 'none',
+	};
 }
 
 /**
@@ -172,9 +176,8 @@ export function getCss(style, theme, outline) {
  * @returns Number
  */
 function oppositeValueInRange(num, min, max) {
-    return min + (max - num);
+	return min + (max - num);
 }
-
 
 /**
  * Get brightness value of color
@@ -182,15 +185,15 @@ function oppositeValueInRange(num, min, max) {
  * @returns Float
  */
 function luminance(color) {
-    const r = color.r;
-    const g = color.g;
-    const b = color.b;
+	const r = color.r;
+	const g = color.g;
+	const b = color.b;
 
-    var a = [r, g, b].map(function (v) {
-        v /= 255;
-        return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
-    });
-    return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+	var a = [r, g, b].map(function (v) {
+		v /= 255;
+		return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
+	});
+	return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
 }
 
 /**
@@ -200,9 +203,9 @@ function luminance(color) {
  * @returns Float
  */
 function contrast(color1, color2) {
-    var lum1 = luminance(color1);
-    var lum2 = luminance(color2);
-    var brightest = Math.max(lum1, lum2);
-    var darkest = Math.min(lum1, lum2);
-    return (brightest + 0.05) / (darkest + 0.05);
+	var lum1 = luminance(color1);
+	var lum2 = luminance(color2);
+	var brightest = Math.max(lum1, lum2);
+	var darkest = Math.min(lum1, lum2);
+	return (brightest + 0.05) / (darkest + 0.05);
 }
